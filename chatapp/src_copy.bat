da666e72f248f79bede9d0ecd25834383b68ec95
PRISM-DESKTOP\prismdata <theprismdata@gmail.com>
2025-02-16 14:58:57 +0900

    dummy test

--- chatapp/src_copy.bat
+++ chatapp/src_copy.bat
@@ -3,7 +3,7 @@ rem chatapp_src 폴더가 없으면 생성
 if not exist chatapp_src mkdir chatapp_src
 
 rem nginx 폴더의 파일들 복사
-copy nginx\nginx.dev.conf chatapp_src\
+copy nginx\nginx.conf chatapp_src\
 
 rem src 폴더의 js, jsx 파일들 복사
 for /r "src" %%f in (*.js) do (
@@ -34,6 +34,7 @@ for /r "src" %%f in (*.jsx) do (
     echo %%~nxf | findstr /i "MessageInput.jsx" > nul
     if errorlevel 1 copy "%%f" chatapp_src\
 )
+
 
 rem chatapp 폴더의 json 파일과 dockerfile 복사
 copy package.json chatapp_src\
